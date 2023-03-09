07177184ae057b6a048841c830c1b53735fd9716
allainzenith <allainzenith12@gmail.com>
2023-03-10 03:39:31 +0800

    added api for leasing ang messages

--- app.py
+++ app.py
@@ -3,19 +3,32 @@ from flask_mysqldb import MySQL
 from flask_restful import Api,Resource
 import db
 import restapi
+from flask_cors import CORS
+from flask_socketio import SocketIO, send, emit
+import requests
+
 
 app = Flask(__name__)
-app.secret_key = "b-lease2022"
 
+
+app.secret_key = "b-lease2022"
 #Api setup
 #-----------------------------------------------------
 
-
+CORS(app)
 api = Api(app)
+#CORS(app, origins="http://localhost:8100")
+
+#socketio = SocketIO(app, cors_allowed_origins="*")
+#----------------------------------------------------
+
 api.add_resource(restapi.user,"/user")
 # api.add_resource(restapi.signup,"/signup/<string:id>")
 
+api.add_resource(restapi.Leasing,"/leasing")
+api.add_resource(restapi.Leasing_Documents,"/leasingdocs")
 
+api.add_resource(restapi.Message,"/message")
 #-----------------------------------------------------
 
 #Database Connection Setup 
@@ -31,13 +44,15 @@ mysql = MySQL(app)
 
 
 
-
-
-
-
-
-    
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     db.create_all()
+#     socketio.run(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
